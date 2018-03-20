7c8fefc78140a1fe43068213be68c0410e4e868a
anvien <anrial@126.com>
2018-03-20 22:36:57 +0800

    Update MultiClusteredReservoirWeightsGenerator.py

--- weights_generator/reservoir_weights/MultiClusteredReservoirWeightsGenerator.py
+++ weights_generator/reservoir_weights/MultiClusteredReservoirWeightsGenerator.py
@@ -1,7 +1,7 @@
 from weights_generator.reservoir_weights.ReservoirWeightsGenerator import ReservoirWeightsGenerator
 
 
-class RandomReservoirWeightsGenerator(ReservoirWeightsGenerator):
+class MultiClusteredReservoirWeightsGenerator(ReservoirWeightsGenerator):
     def __init__(self):
         ReservoirWeightsGenerator.__init__(self)
 
